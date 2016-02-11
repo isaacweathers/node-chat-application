b73f79bbb83ebeccfb836f1eafa0a06dd86b0805
Miles Coleman <miles.lcoleman@gmail.com>
2016-02-10 18:25:20 -0600

    Added user schema. Needs work.

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var http = require('http');
+var mongoose = require('mongoose'), Schema = mongoose.Schema;
+var express = require('express');
+
+var userSchema = new Schema({
+	username: {type: String},
+	password: {type: String}
+});
 
 function handleRequest(request, response) {
 	response.end('It works!' + request.url);
 }
+
 
 var server = http.createServer(handleRequest);
 
