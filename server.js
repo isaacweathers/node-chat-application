ca18f03c051fb9b6207c686b6bbb959bb566c64c
Miles Coleman <miles.lcoleman@gmail.com>
2016-02-16 02:56:21 -0600

    Added in login sessions with passport

--- server.js
+++ server.js
@@ -8,16 +8,47 @@ var bodyParser = require('body-parser');
 var User = mongoose.model('User', userModule.userSchema);
 mongoose.connect('mongodb://localhost/test');
 var db = mongoose.connection;
-
+var passport = require('passport'), LocalStrategy = require('passport-local').Strategy;
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
 // Application settings
 
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
 app.use(express.static('bower_components'));
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+app.use(session({
+	secret: 'keyboard cat'
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+passport.use(
+	new LocalStrategy(
+		function (username, password, done) {
+			userModule.userSchema.methods.login(username, password, function(response) {
+				if (typeof(response) !== 'object') {
+					done(null, false, {message: "Invalid credentials."});
+				} else {
+					done(null, response);
+				}
+			});
+		}
+	)
+);
+
+passport.serializeUser(function(user, done) {
+	done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+	done(null, user);
+});
 
 // ROUTING
 
@@ -31,10 +62,17 @@ app.post('/register-action', function(req, res) {
 	});
 });
 
-app.post('/login-action', function(req, res) {
-	userModule.userSchema.methods.login(req.body.username, req.body.password, function(response) {
-		res.send(200, response);
-	});
+app.post('/login-action', passport.authenticate('local', {successRedirect: '/success', failureRedirect: '/failure'}),function(req, res) {
+
 });
+
+app.get('/success', function(req, res) {
+	res.send({status: 'OK'});
+});
+
+app.get('/failure', function(req, res) {
+	console.log(req.user);
+	res.send("");
+})
 
 app.listen(8000);
